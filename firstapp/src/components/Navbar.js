8b48433820e96f1dbb7fb666024bf8290ad30dc5
NIKHIL JI <taparianikhil13@gmail.com>
2021-12-15 17:49:18 +0530

    proptypes and default props

--- firstapp/src/components/Navbar.js
+++ firstapp/src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function Navbar() {
+
+export default function Navbar(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Navbar</a>
+    <a className="navbar-brand" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -39,4 +41,12 @@ export default function Navbar() {
   </div>
 </nav>
     )
+}
+
+Navbar.propTypes = {
+    title : PropTypes.string
+}
+
+Navbar.defaultProps = {
+    title : 'Gamer'
 }
