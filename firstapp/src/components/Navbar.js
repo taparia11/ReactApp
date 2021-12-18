059f1b2f38974862c9909a40f97b547b428b449b
NIKHIL JI <taparianikhil13@gmail.com>
2021-12-18 21:34:57 +0530

    Dark mode
    dark mode added

--- firstapp/src/components/Navbar.js
+++ firstapp/src/components/Navbar.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
     <a className="navbar-brand" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,13 +20,13 @@ export default function Navbar(props) {
         </li>
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Functions
           </a>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
+            <li><a className="dropdown-item" href="/">Upper Case</a></li>
+            <li><a className="dropdown-item" href="/">Lower Case</a></li>
             <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
+            <li><a className="dropdown-item" href="/">Addittional Features</a></li>
           </ul>
         </li>
         <li className="nav-item">
@@ -35,7 +35,7 @@ export default function Navbar(props) {
       </ul>
       <form className="d-flex">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <button className="btn btn-outline-primary" type="submit">Search</button>
       </form>
     </div>
   </div>
--- firstapp/src/components/Textform.js
+++ firstapp/src/components/Textform.js
@@ -3,8 +3,14 @@ import React, {useState} from 'react'
 
 export default function Textform(props) {
     
+    
     const handleUpClick =()=>{
         let newText = text.toUpperCase();
+        setText(newText)
+    }
+
+    const handleLoClick =()=>{
+        let newText = text.toLowerCase();
         setText(newText)
     }
     
@@ -20,11 +26,14 @@ export default function Textform(props) {
                     <label htmlFor="mybox" className="form-label"><h1>{props.heading}</h1></label>
                     <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="4"></textarea>
                 </div>
-                <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+                <button className="btn btn-primary mx-2 my-3" onClick={handleLoClick}>Convert to lowerCase</button>
             </div>
             <div className="container my-4">
                 <h1>Your text summary</h1>
-                <p>{text.length} characters and {text.split(" ").length} words</p>
+                <p>{text.length - text.split(" ").length +1} characters and {text.split(" ").length} words</p>
+            <h1>Preview</h1>
+            <p>{text}</p>
             </div>
         </>
     );
