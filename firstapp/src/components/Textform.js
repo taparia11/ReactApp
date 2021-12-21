d13fa32b2df2161fb7e248e3d1b53d65eda4199c
NIKHIL JI <taparianikhil13@gmail.com>
2021-12-21 18:11:04 +0530

    updated

--- firstapp/src/components/Textform.js
+++ firstapp/src/components/Textform.js
@@ -7,12 +7,19 @@ export default function Textform(props) {
     const handleUpClick =()=>{
         let newText = text.toUpperCase();
         setText(newText)
-        props.showAlert('Text has been changed to capital','success');
+        props.showAlert('Converted to Uppercase','success');
     }
-
+    
     const handleLoClick =()=>{
         let newText = text.toLowerCase();
         setText(newText)
+        props.showAlert('Converted to Lowercase','success');
+    }
+    
+    const handleClear = () =>{
+        let newtext = '';
+        setText(newtext);
+        props.showAlert('Cleared','success');
     }
     
     const handleOnChange =(event)=>{
@@ -43,12 +50,13 @@ export default function Textform(props) {
                 <button className="btn btn-primary mx-2 my-3" onClick={handleLoClick}>Convert to lowerCase</button>
                 <button className="btn btn-primary mx-2 my-3" onClick={handleCopy}>Copy Text</button>
                 <button className="btn btn-primary mx-2 my-3" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+                <button className="btn btn-danger mx-2 my-3" onClick={handleClear}>Clear</button>
             </div>
             <div className="container my-4" style={{color: props.mode==='light'?'black':'white'}}>
                 <h1>Your text summary</h1>
                 <p>{text.length - text.split(" ").length +1} characters and {text.split(" ").length} words</p>
             <h1>Preview</h1>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Nothing to preview!"}</p>
             </div>
         </>
     );
