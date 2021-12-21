7ad611b072f84927e30aeaffc4892a3b0b319cb5
NIKHIL JI <taparianikhil13@gmail.com>
2021-12-21 17:09:11 +0530

    Alert has been completed

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -4,26 +4,38 @@ import React, { useState } from 'react'
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import Alert from './components/Alert';
 
 //let nikhil = "Nikhil Taparia"
 
 function App() {
 
   const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type:type 
+    })
+  }
 
   const toggleMode = ()=>{
     if(mode==='light'){
       setMode('dark')
       document.body.style.backgroundColor = '#042743'
+      showAlert('Dark Mode has been Enabled', 'success');
     }
     else{
       setMode( 'light')
       document.body.style.backgroundColor = 'white'
+      showAlert('Light Mode has been Enabled', 'success');
     }
   }
   return (
     <>
       <Navbar title="TextFormator" mode ={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
       <div className="container my-4">
       {/* <About/> */}
       <Textform heading="Enter Text Below" mode ={mode}/>
