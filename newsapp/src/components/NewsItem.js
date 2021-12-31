3a276d8f48dc201e76803885a38379c43f24a8d4
taparia11 <taparianikhil13@gmail.com>
2021-12-31 12:53:42 +0530

    updated

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react'
-
+// import Button from '@mui/material/Button';
 
 export default class NewsItem extends Component {
+
+   
+
     render() {
-        let {title, description, imageUrl} = this.props;
+        let {title, description, imageUrl, newsUrl} = this.props;
         return (
             <div className='my-3'>
                 <div className="card" style={{width: "18rem"}}>
                     <img src={imageUrl} className="card-img-top" alt="..."/>
                         <div className="card-body">
-                            <h5 className="card-title">{title}</h5>
-                            <p className="card-text">{description}</p>
-                            <a href="/" className="btn btn-sm btn-primary">Read More</a>
+                            <h5 className="card-title">{title}...</h5>
+                            <p className="card-text">{description}...</p>
+                            <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
+                            {/* <Button href="#text-buttons" variant="contained" size='small'>Read More</Button> */}
+
                         </div>
                 </div>
             </div>
