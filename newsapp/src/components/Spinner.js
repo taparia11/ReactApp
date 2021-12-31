4c0e58645357cb509a004b711f8332c37cb181ff
taparia11 <taparianikhil13@gmail.com>
2021-12-31 12:54:23 +0530

    Loading spinner added and implimented

---
+++ newsapp/src/components/Spinner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import spinner from './spinner.gif'
+
+
+export default class Spinner extends Component {
+    render() {
+        return (
+            <div className='text-center'>
+                <img src={spinner} alt="Khebar Aapki" />
+            </div>
+            
+        )
+    }
+}
