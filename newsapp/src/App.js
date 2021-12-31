517813f304940d64eb5b2b9a61b6718c72baf80c
taparia11 <taparianikhil13@gmail.com>
2021-12-31 20:41:38 +0530

    bugs fixed

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -8,19 +8,20 @@ import './App.css';
 
 
 export default class App extends Component {
+   pageSize = 15;
 
   render() {
     return (
       <Router>
         <NavBar/>
         <Routes>
-        <Route exact path='/' element={<News key='general' pageSize={5} country="in" category="general"/>}/>
-        <Route exact path='/business' element={<News key="business" pageSize={5} country="in" category="business"/>}/>
-        <Route exact path='/entertainment' element={<News key="entertainment" pageSize={5} country="in" category="entertainment"/>}/>
-        <Route exact path='/health' element={<News key='health' pageSize={5} country="in" category="health"/>}/>
-        <Route exact path='/science' element={<News key='science' pageSize={5} country="in" category="science"/>}/>
-        <Route exact path='/sports' element={<News key='sports' pageSize={5} country="in" category="sports"/>}/>
-        <Route exact path='/technology' element={<News key='technology' pageSize={5} country="in" category="technology"/>}/>
+        <Route exact path='/' element={<News key='General' pageSize={this.pageSize} country="in" category="general" titleChange="Top Headlines"/>}/>
+        <Route exact path='/business' element={<News key="business" pageSize={this.pageSize} country="in" category="business" titleChange="Business"/>}/>
+        <Route exact path='/entertainment' element={<News key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" titleChange="Entertainment"/>}/>
+        <Route exact path='/health' element={<News key='health' pageSize={this.pageSize} country="in" category="health" titleChange="Health"/>}/>
+        <Route exact path='/science' element={<News key='science' pageSize={this.pageSize} country="in" category="science" titleChange="Science"/>}/>
+        <Route exact path='/sports' element={<News key='sports' pageSize={this.pageSize} country="in" category="sports" titleChange="Sports"/>}/>
+        <Route exact path='/technology' element={<News key='technology' pageSize={this.pageSize} country="in" category="technology" titleChange="Technology"/>}/>
         
         </Routes>
       </Router>
