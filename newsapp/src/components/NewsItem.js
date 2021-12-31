677b3b1bcb0ea692e338c2a52c9fc41ab37a9e04
taparia11 <taparianikhil13@gmail.com>
2021-12-31 20:42:13 +0530

    updated

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -6,14 +6,16 @@ export default class NewsItem extends Component {
    
 
     render() {
-        let {title, description, imageUrl, newsUrl} = this.props;
+        let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
         return (
             <div className='my-3'>
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imageUrl} className="card-img-top" alt="..."/>
+                    <img src={imageUrl} className="card-img-top" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">{title}...</h5>
+                            <h5 className="card-title">{title}...<span className="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-danger">{source}</span></h5>
                             <p className="card-text">{description}...</p>
+                            <p className='card-text'><small className='text-muted'>By {author} on {new Date(date).toGMTString()}</small>
+                            </p>
                             <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
                             {/* <Button href="#text-buttons" variant="contained" size='small'>Read More</Button> */}
 
