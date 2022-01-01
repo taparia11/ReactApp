8d352d12ff55466e1b0fefda924aee1baaf67183
taparia11 <taparianikhil13@gmail.com>
2022-01-01 14:44:28 +0530

    spaces removed

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -4,17 +4,13 @@ import News from './components/News';
 import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom'
 import LoadingBar from 'react-top-loading-bar'
 
-
 import './App.css';
  
-
 export default function App () {
    let pageSize = 15;
    let apiKey = "7fad0c0d80714e99b458b2c602716dbb"
   //  apiKey = "f97bdaf5b38c1143dbd503873ece9987"
   //  apiKey = process.env.REACT_APP_NEWS_API
-
-  
 
    const [progress, setProgress] = useState(0)
 
@@ -34,8 +30,7 @@ export default function App () {
         <Route exact path='/health' element={<News setProgress={setProgress} apiKey={apiKey} key='health' pageSize={pageSize} country="in" category="health" titleChange="Health"/>}/>
         <Route exact path='/science' element={<News setProgress={setProgress} apiKey={apiKey} key='science' pageSize={pageSize} country="in" category="science" titleChange="Science"/>}/>
         <Route exact path='/sports' element={<News setProgress={setProgress} apiKey={apiKey} key='sports' pageSize={pageSize} country="in" category="sports" titleChange="Sports"/>}/>
-        <Route exact path='/technology' element={<News setProgress={setProgress} apiKey={apiKey} key='technology' pageSize={pageSize} country="in" category="technology" titleChange="Technology"/>}/>
-        
+        <Route exact path='/technology' element={<News setProgress={setProgress} apiKey={apiKey} key='technology' pageSize={pageSize} country="in" category="technology" titleChange="Technology"/>}/>   
         </Routes>
       </Router>
     )
