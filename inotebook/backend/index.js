3a55566dcba3c4d6d6db24e5bea31ece4fb8596d
taparia11 <taparianikhil13@gmail.com>
2022-01-01 21:06:22 +0530

    request and response setup

---
+++ inotebook/backend/index.js
@@ -0,0 +1,18 @@
+const connectToMongo = require('./db');
+const express = require('express')
+
+connectToMongo();
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+// app.get('/', (req, res) => {
+//   res.send('Hello NIKHIL JI')
+// })
+
+app.use('/api/auth', require('./routes/auth'))
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
