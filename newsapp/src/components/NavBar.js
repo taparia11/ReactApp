4f46c82027e9282c2fe367d7b20ad72a09bbd7d7
taparia11 <taparianikhil13@gmail.com>
2022-01-01 11:36:45 +0530

    changed to funtion based components

--- newsapp/src/components/NavBar.js
+++ newsapp/src/components/NavBar.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-export default class NavBar extends Component {
-    render() {
+export default function NavBar () {
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -15,6 +14,9 @@ export default class NavBar extends Component {
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
           <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link text-danger" to="/election">Election</Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link text-warning" to="/business">Business</Link>
@@ -44,5 +46,5 @@ export default class NavBar extends Component {
 </nav>
             </div>
         )
-    }
+    
 }
