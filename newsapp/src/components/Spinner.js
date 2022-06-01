dd243c387b081f894be50fff441b708411a31418
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-06-01 22:39:04 +0530

    details updated

--- newsapp/src/components/Spinner.js
+++ newsapp/src/components/Spinner.js
@@ -5,6 +5,8 @@ import spinner from './spinner.gif'
 const Spinner = () => {
    
         return (
+
+            //loading spinner
             <div className='text-center'>
                 <img className="my-3" src={spinner} alt="Khebar Aapki" />
             </div>
