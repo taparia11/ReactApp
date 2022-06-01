abb98623475c031c6b26556ef028d5916801a4a6
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-06-01 22:38:52 +0530

    Comment added

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -5,7 +5,7 @@ import React from 'react'
 
    
 
-   
+        // geting these data from props
         let {title, description, imageUrl, newsUrl, author, date, source} = props;
         return (
             <div className='my-3'>
